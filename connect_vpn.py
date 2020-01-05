40255ef0da0289e0fa9698475960bd3d21de1064
Mike Watts <awarebayes@protonmail.com>
2020-01-05 23:29:31 +0300

    vpn changed

--- connect_vpn.py
+++ connect_vpn.py
@@ -1,12 +1,14 @@
 import subprocess
 
-def run_shell(command):
+def run_shell(command, debug=True):
+    if debug:
+        print('running\n', command)
     output = subprocess.run(command, stdout=subprocess.PIPE,
                             text=True, shell=True).stdout
     return output
 
 def list_vpn():
-    output = run_shell("nmcli connection show")
+    output = run_shell("nmcli connection show", False)
     names = output.split('\n')
     names = list(filter(lambda i: 'vpn' in i, names))
     devices = list(map(lambda i: i.split()[3], names))
@@ -23,8 +25,16 @@ def main():
     vpns, _ = list_vpn()
     names_pretty = list(map(lambda i: i.split('.')[0], vpns))
     names_pretty = ["disconnect"] + names_pretty
-    choice = run_shell('echo -e "{}" | dmenu'.format('\n'.join(names_pretty)))
+    choice = run_shell('echo -e "{}" | dmenu -l {} -fn {}'.format(
+                       '\n'.join(names_pretty),
+                       len(names_pretty),
+                       "mono:pixelsize=20:antialias=true",
+                       ), False)
     choice = choice[:-1] # del traiting \n
+    
+    if choice == '':
+        return
+    
     choice_idx = names_pretty.index(choice)
     if choice_idx == 0:
         disconnect_all()
@@ -32,3 +42,4 @@ def main():
         run_shell('nmcli connection up {}'.format(vpns[choice_idx-1]))
 
 main()
+
